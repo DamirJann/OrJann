efda579c7b86a95714ef699ceda59f3aedcbadbc
Дамир <pilacis@list.ru>
2018-12-18 21:40:09 +0300

    Вариант 1.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,49 @@
 #include <iostream>
 #include <fstream>
+#include <windows.h>
 using namespace std;
 
 const int hight = 4;
 const int width = 4;
 
-void movement(char& );
 
 struct character{
     int X;
     int Y;
 };
+bool check(int X, int Y, char**);
+void movement(char&, character&, char **);
+void print_field(char **);
 
 
 int main() {
-    ifstream cin("input.txt");
-    char field[hight][hight];
+    ifstream cin_file("input.txt");
     int i, j;
-    character hero = {1,1};
+    character hero = {0,0};
+
+    // СОЗДАНИЕ ДВУМЕРНОГО МАССИВА
+    char **field = new char* [hight];
+    for (i = 0; i < hight; i++){
+        field[i] = new char [width];
+    }
 
     // ЗАПОЛНЕНИЕ МАССИВА
     for (i = 0; i < hight; i++){
         for (j = 0; j < width; j++){
-            cin >> field[i][j];
+            cin_file >> field[i][j];
         }
     }
+
 
     // СЧИТЫВАНИЕ ПЕРЕДВИЖЕНИЙ
     while (true){
         char direction;
         cin >> direction;
+        movement(direction, hero, field);
         system("cls");
-        print_field();
+        print_field(field);
+        //cout << endl << endl;
+
     }
 
 
@@ -39,21 +51,57 @@ int main() {
 }
 
 
-void movement(char& direction, character hero){
+bool check(int Y, int X, char **field){
+    if ((X < 0) || (X >= width) || (Y < 0) || (Y >= hight) || (field[Y][X] != '_')){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+void movement(char& direction, character& hero, char **field){
     switch (direction){
-        case 'A':
-            if (check(hero.X-1, hero.Y)){
-
+        case 'a':
+            if (check(hero.Y, hero.X-1, field)){
+                field[hero.Y][hero.X] = '_';
+                field[hero.Y][hero.X-1] = '!';
+                hero.X--;
             }
+            break;
 
+        case 'd':
+            if (check(hero.Y, hero.X+1, field)){
+                field[hero.Y][hero.X] = '_';
+                field[hero.Y][hero.X+1] = '!';
+                hero.X++;
+            }
             break;
-        case 'B':
+
+        case 'w':
+            if (check(hero.Y-1, hero.X, field)){
+                field[hero.Y][hero.X] = '_';
+                field[hero.Y-1][hero.X] = '!';
+                hero.Y--;
+            }
             break;
-        case 'C':
-            break;
-        case 'D':
+
+        case 's':
+            if (check(hero.Y+1, hero.X, field)){
+                field[hero.Y][hero.X] = '_';
+                field[hero.Y+1][hero.X] = '!';
+                hero.Y++;
+            }
             break;
 
     }
 
+}
+
+void print_field(char **field){
+    for (int i = 0; i < hight; i++){
+        cout << endl;
+        for (int j = 0; j < width; j++){
+            cout << field[i][j];
+        }
+    }
 }
