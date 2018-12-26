ad1a54dc2fc74ead8518b71c77984834eebae066
Дамир <pilacis@list.ru>
2018-12-26 21:08:24 +0300

    Version 4 1) Добавил алгоритм Ли, который знает все кратчайшие    пути от игрока до любой точки 2) Изменил функции проверки на возможность кода, добавил вывод массива int.

--- main.cpp
+++ main.cpp
@@ -2,53 +2,87 @@
 #include <fstream>
 #include <conio.h>
 #include <windows.h>
+#include <queue>
 using namespace std;
-
-const int hight = 10;
-const int width = 10;
 
 #define KEY_UP 'W'
 #define KEY_DOWN 'S'
 #define KEY_LEFT 'A'
 #define KEY_RIGHT 'D'
 #define ESCAPE 27
+#define WALL 9
+#define INFINITY 8
 
-
-struct character{
+struct point{
     int Y;
     int X;
 };
-void check_move(character&, character, char**);
-void movement(int&, character&, char **);
-void print_field(char **);
+
+void corect_coordinates(point&, int, int);
+void check_move(point&, point, char**, int, int);
+void movement(int&, point&, char **, int, int);
+
+void print_field(char **, int, int);
+void print_field(int **, int, int);
+
 char get_direction();
+
+
+void Lee_algorithm(int **, char **, queue<point>&, int, int, int);
+
+
 
 
 int main() {
     ifstream cin_file("input.txt");
     ofstream cout_file("output.txt");
     int i, j;
-    character hero = {0,0};
-    // СОЗДАНИЕ ДВУМЕРНОГО МАССИВА
+    int hight = 30;
+    int width = 20;
+    point hero = {0,0};
+
+    // СОЗДАНИЕ ДВУМЕРНОГО МАССИВА КАРТЫ И КОЛИЧЕСТВА ШАГОВ ДО КЛЕТОК
     char **field = new char* [hight];
+    int **step_field = new int* [hight];
     for (i = 0; i < hight; i++){
         field[i] = new char [width];
+        step_field[i] = new int [width];
     }
 
     // ЗАПОЛНЕНИЕ МАССИВА
     for (i = 0; i < hight; i++){
         for (j = 0; j < width; j++){
             cin_file >> field[i][j];
+            switch (field[i][j]){
+                case '#':
+                    step_field[i][j] = WALL;
+                    break;
+                case '_':
+                    step_field[i][j] = INFINITY;
+                    break;
+                default:
+                    step_field[i][j] = 0;
+                    break;
+            }
         }
     }
+
+
+    // ЗАПУСК АЛГОРИТМА ЛИ и объявление вектора вызовов
+    queue<point> call_queue;
+    call_queue.push({0, 0});
+    Lee_algorithm(step_field, field, call_queue, hight, width, 1);
+    // print_field(step_field, hight, width);
+
+
     // СЧИТЫВАНИЕ ПЕРЕДВИЖЕНИЙ
     bool not_escape = true;
     while(not_escape){
-        print_field(field);
+        print_field(field, hight, width);
         int direction = get_direction();
         not_escape = (direction != ESCAPE);
         if (not_escape) {
-            movement(direction, hero, field);
+            movement(direction, hero, field, hight, width);
             system("cls");
         }
     }
@@ -57,13 +91,32 @@ int main() {
 }
 
 
+
+
+
+void corect_coordinates(point& p, int hight, int width){
+    if (p.X == -1) p.X = width-1;
+    if (p.X == width) p.X = 0;
+
+    if (p.Y == -1) p.Y = hight-1;
+    if (p.Y == hight) p.Y = 0;
+}
+
+bool check(int  ** step_field, int hight, int width, point p){
+    corect_coordinates(p, hight, width);
+    if (step_field[p.Y][p.X] == INFINITY )
+        return true;
+    else
+        return false;
+
+}
+
 char get_direction(){
     int i = getch();
 
     if (i == 224) {
         i = getch();
         switch (i) {
-
             case 72:
                 return 'W';
                 break;
@@ -91,30 +144,15 @@ char get_direction(){
 
 }
 
-void check_move(character& old_rotate, character new_rotate, char **field){
+
+
+
+
+
+void check_move(point& old_rotate, point new_rotate, char **field, int hight, int width){
 
     // ОБРАБОТКА ВЫХОДА ЗА ГРАНИЦУ КАРТЫ
-    switch (new_rotate.X){
-        case -1:
-            new_rotate.X = width - 1;
-            break;
-        case width:
-            new_rotate.X = 0;
-            break;
-        default:
-            break;
-    }
-
-    switch (new_rotate.Y){
-        case -1:
-            new_rotate.Y = hight - 1;
-            break;
-        case hight:
-            new_rotate.Y = 0;
-            break;
-        default:
-            break;
-    }
+    corect_coordinates(new_rotate, hight, width);
 
     if (field[new_rotate.Y][new_rotate.X] == '_'){
         field[new_rotate.Y][new_rotate.X] = '!';
@@ -122,22 +160,22 @@ void check_move(character& old_rotate, character new_rotate, char **field){
         old_rotate = new_rotate;
     }
 }
-void movement(int& direction, character& hero, char **field){
+void movement(int& direction, point& hero, char **field, int hight, int width){
     switch (direction) {
         case KEY_LEFT:
-            check_move(hero, {hero.Y, hero.X - 1}, field);
+            check_move(hero, {hero.Y, hero.X - 1}, field, hight, width);
             break;
 
         case KEY_RIGHT:
-            check_move(hero, {hero.Y, hero.X + 1}, field);
+            check_move(hero, {hero.Y, hero.X + 1}, field, hight, width);
             break;
 
         case KEY_UP:
-            check_move(hero, {hero.Y - 1, hero.X}, field);
+            check_move(hero, {hero.Y - 1, hero.X}, field, hight, width);
             break;
 
         case KEY_DOWN:
-            check_move(hero, {hero.Y + 1, hero.X}, field);
+            check_move(hero, {hero.Y + 1, hero.X}, field, hight, width);
             break;
 
         default:
@@ -145,11 +183,72 @@ void movement(int& direction, character& hero, char **field){
     }
 }
 
-void print_field(char **field){
+void print_field(char **field, int hight, int width){
     for (int i = 0; i < hight; i++){
         cout << endl;
         for (int j = 0; j < width; j++){
             cout << field[i][j];
         }
     }
+}
+
+void print_field(int **massive, int hight, int width){
+    for (int i = 0; i < hight; i++){
+        cout << endl;
+        for (int j = 0; j < width; j++){
+            cout << massive[i][j];
+        }
+    }
+}
+
+void Lee_algorithm(int ** step_field, char ** field, queue<point>& call_queue, int hight, int width, int weawe_count){
+
+
+    int count = call_queue.size();
+
+    while (count != 0) {
+
+
+        // КООРДИНАТЫ РАССМАТРИВАЕМОГО ЯДРА
+        int x = call_queue.front().X;
+        int y = call_queue.front().Y;
+        point p;
+
+        p = {y-1, x};
+        if (check(step_field, hight, width, p) == true) {
+            corect_coordinates(p, hight, width);
+            call_queue.push(p);
+            step_field[p.Y][p.X] = weawe_count;
+        }
+
+        p = {y+1, x};
+        if (check(step_field, hight, width, p) == true) {
+            corect_coordinates(p, hight, width);
+            call_queue.push(p);
+            step_field[p.Y][p.X] = weawe_count;
+        }
+
+        p = {y, x-1};
+        if (check(step_field, hight, width, p) == true) {
+            corect_coordinates(p, hight, width);
+            call_queue.push(p);
+            step_field[p.Y][p.X] = weawe_count;
+        }
+
+        p = {y, x+1};
+        if (check(step_field, hight, width, p) == true) {
+            corect_coordinates(p, hight, width);
+            call_queue.push(p);
+            step_field[p.Y][p.X] = weawe_count;
+        }
+
+        call_queue.pop();
+        count--;
+    }
+
+    if (!call_queue.empty()){
+        Lee_algorithm(step_field, field, call_queue, hight, width, weawe_count+1);
+    }
+
+
 }
